910aeb9524891f0274df4b138bcb9cfdea1588e7
April <aprilwang@cs.uchicago.edu>
2019-05-11 16:57:37 -0500

    finally got the div

---
+++ browser-extension/youtubeScript.js
@@ -0,0 +1,11 @@
+
+
+//content = document.getElementById("content");
+num = document.getElementsByTagName("ytd-page-manager");
+console.log(num);
+//.getElementsByClassName("style-scope ytd-page-manager hide-skeleton");
+alert("hello1");
+
+// x = divChild.getElementById("columns").getElementById("primary").getElementById("primary-inner").getElementById("related");
+// y = x.getElementsByClassName("style-scope ytd-watch-flexy").getElementById("style-scope ytd-watch-next-secondary-results-renderer")[1];
+// vid = y.getElementById("dismissable").getElementsByClassName("style-scope ytd-compact-video-renderer");
