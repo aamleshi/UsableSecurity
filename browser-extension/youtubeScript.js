42a6d2304ac9ba30f610484e008ef47a05536c01
April <aprilwang@cs.uchicago.edu>
2019-05-13 00:02:21 -0500

    added jquery library. note- ??

--- browser-extension/youtubeScript.js
+++ browser-extension/youtubeScript.js
@@ -1,8 +1,27 @@
 
 
 //content = document.getElementById("content");
-num = document.getElementsByTagName("ytd-page-manager");
-console.log(num);
+// num = document.getElementsByTagName("ytd-page-manager");
+// console.log(num);
+// flexy = document.getElementsByTagName("ytd-watch-flexy");
+// console.log(flexy);
+
+// //children = flexy[0].childNodes;
+// console.log(flexy.nodeName);
+
+//console.log(children.length);
+
+$(document).ready(function(){
+	console.log($("#ytd-page-manager"));
+
+
+	// $( "ytd-watch-flexy" ).each(function( index ) {
+	//   console.log( index + ": " + $( this ).text() );
+	// });
+
+});
+
+
 //.getElementsByClassName("style-scope ytd-page-manager hide-skeleton");
 alert("hello1");
 
