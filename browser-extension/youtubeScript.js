6159ee3420e02761cf3c12e36b3125781379913d
April <aprilwang@cs.uchicago.edu>
2019-05-13 19:57:22 -0500

    got videos with jquery

--- browser-extension/youtubeScript.js
+++ browser-extension/youtubeScript.js
@@ -1,30 +1,11 @@
+/* using jquery */
+$(document).ready(function(){ 
+	//$('ytd-app').
 
+	//var ytd_app_obj = document.getElementsByTagName('ytd-app');
+	//console.log($(ytd_app_obj).children('#content.style-scope.ytd-app'));
+	//console.log($("#items").find("ytd-watch-next-secondary-results-renderer"));
+	console.log($('ytd-compact-video-renderer'));
 
-//content = document.getElementById("content");
-// num = document.getElementsByTagName("ytd-page-manager");
-// console.log(num);
-// flexy = document.getElementsByTagName("ytd-watch-flexy");
-// console.log(flexy);
-
-// //children = flexy[0].childNodes;
-// console.log(flexy.nodeName);
-
-//console.log(children.length);
-
-$(document).ready(function(){
-	console.log($("#ytd-page-manager"));
-
-
-	// $( "ytd-watch-flexy" ).each(function( index ) {
-	//   console.log( index + ": " + $( this ).text() );
-	// });
 
 });
-
-
-//.getElementsByClassName("style-scope ytd-page-manager hide-skeleton");
-alert("hello1");
-
-// x = divChild.getElementById("columns").getElementById("primary").getElementById("primary-inner").getElementById("related");
-// y = x.getElementsByClassName("style-scope ytd-watch-flexy").getElementById("style-scope ytd-watch-next-secondary-results-renderer")[1];
-// vid = y.getElementById("dismissable").getElementsByClassName("style-scope ytd-compact-video-renderer");
