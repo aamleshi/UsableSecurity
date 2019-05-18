c15164d03b114455aa85f19ee8ced785f28855ad
April <aprilyanli38@gmail.com>
2019-05-17 19:19:10 -0500

    some work on generate_rec

--- browser-extension/youtubeScript0.js
+++ browser-extension/youtubeScript0.js
@@ -73,3 +73,22 @@ $(document).ready(function(){
 
 });
 
+
+
+/* may 16, thumbnail replacement & after document has loaded */
+
+var f = (function(e){ 
+
+	console.log(e)
+	videos = document.querySelector('#related #items').children;
+
+
+	vid = videos[1];
+	console.log(vid);
+	console.log(vid.getElementsByTagName('img'));
+	thumbnail = vid.getElementsByTagName('img')[0];
+	thumbnail.src = 'https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80';
+
+});
+
+setTimeout(f, 5000);
