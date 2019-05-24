e49178c3b5f95c9ddc42fa19e05b6aac70597a69
Arresh Amleshi <aamleshi@uchicago.edu>
2019-05-24 15:07:23 -0500

    fixed script to deal with yt time tracking

--- updateYoutubeRecs.py
+++ updateYoutubeRecs.py
@@ -4,6 +4,51 @@ from utils import keyword_to_videos
 import time
 from tqdm import tqdm
 
+"""
+# TODO:
+Mark visited endpoint rows as visited
+Add rate-limiting failsafes?
+"""
+
+
+def updateTime(client):
+    yotubeTimeEndpoint = client.open('YoutubeTimeForm (Responses)').get_worksheet(0)
+    RIDdict = {}
+    values = youtubeTimeEndpoint.get_all_values()
+    for row in tqdm(values):
+        if row[2] == '':
+            timestamp = row[0]
+            data = row[1]
+            RID = data[:20]
+            payload = data[20:]
+            print(list(RIDdict.keys()))
+            if RID not in list(RIDdict.keys()):
+                RIDdict[RID] = []
+            RIDdict[RID].append([payload, timestamp])
+    UIDdict = processRIDdictYT(RIDdict)
+    print(UIDdict)
+    for UID in UIDdict.keys():
+        writeToAmzSheet(UIDdict[UID], UID, client)
+
+def processRIDdictAMZ(RIDdict):
+    RIDs = list(RIDdict.keys())
+    UIDdict = {}
+    for RID in RIDs:
+        rawRow = RIDdict[RID]
+        UID = ""
+        Time = ""
+        for element in rawRow:
+            rawElement = element[0]
+            if rawElement[:3] == "UID":
+                UID = rawElement[3:]
+            elif rawElement[:4] == "TIME":
+                Time = rawElement[4:]
+        if UID not in UIDdict:
+            UIDdict[UID] = [[element[1],Time]]
+        else:
+            UIDdict[UID].append([element[1],Time])
+    return UIDdict
+
 def updateAmzHistory(client):
     amazonHistEndpoint = client.open("AmazonHistForm (Responses)").get_worksheet(0)
     RIDdict = {}
@@ -18,13 +63,13 @@ def updateAmzHistory(client):
             if RID not in list(RIDdict.keys()):
                 RIDdict[RID] = []
             RIDdict[RID].append([payload, timestamp])
-    UIDdict = processRIDdict(RIDdict)
+    UIDdict = processRIDdictAMZ(RIDdict)
     print(UIDdict)
     for UID in UIDdict.keys():
         writeToAmzSheet(UIDdict[UID], UID, client)
 
 
-def processRIDdict(RIDdict):
+def processRIDdictAMZ(RIDdict):
     RIDs = list(RIDdict.keys())
     UIDdict = {}
     for RID in RIDs:
@@ -55,14 +100,16 @@ def updateUserRecs(UID, client):
     amazonHistorySheet = client.open(UID).get_worksheet(0)
     youtubeRecSheet = client.open(UID).get_worksheet(1)
     for rowNum in tqdm(range(2,amazonHistorySheet.row_count)):
-        row = amazonHistorySheet.row_values(rowNum) 
+        row = amazonHistorySheet.row_values(rowNum)
         if row == []:
             break
         #print(len(row))
         if len(row) == 4:
             continue
         amazonHistorySheet.update_cell(rowNum, 4, str(time.time()))
-        #print(row[2])
+        #print(row[2])for UID in tqdm(UIDs):
+        #can multithread this if need be
+        updateUserRecs(UID, client)
         videoRecs = keyword_to_videos(row[2])
         #print(videoRecs)
         for item in videoRecs["items"]:
@@ -82,6 +129,9 @@ def main():
     creds = ServiceAccountCredentials.from_json_keyfile_name('owner_creds.json', scope)
     client = gspread.authorize(creds)
     updateAmzHistory(client)
+    for UID in tqdm(UIDs):
+        #can multithread this if need be
+        updateUserRecs(UID, client)
 
 
 if __name__ == '__main__':
