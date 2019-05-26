b53f30f9effbf5fb47664d41a91f46abad51062e
April <aprilyanli38@gmail.com>
2019-05-26 17:46:24 -0500

    href for title

--- browser-extension/youtubeScript.js
+++ browser-extension/youtubeScript.js
@@ -1,53 +1,72 @@
+//global variable
+var random_index = gen_random_index();
+//alert('index is ' + random_index)
+
+function gen_random_index() {
+  return Math.floor(Math.random() * 3) + 1;
+}
 
 /* reload page */
 var currentPage = window.location.href;
 //listen for changes
 setInterval(function()
 {
-    if (currentPage != window.location.href)
-    {
-        // page has changed, set new page as 'current'
-        currentPage = window.location.href;
+  if (currentPage != window.location.href)
+  {
+      // page has changed, set new page as 'current'
+      currentPage = window.location.href;
 
-        // do your thing...
-        console.log('url has changed');
-        location.reload(true);
-    }
+      // do your thing...
+      console.log('url has changed');
+      location.reload(true);
+      random_index = gen_random_index();
+      //alert('index is ' + random_index)
+  }
 }, 500);
 
 
 
 /* helpers */
 var thumbnail_img_idx = [];
+var thumbnail_src_idx = 0;
+var title_href_count = 0;
 
 function get_node_index(addedNode) {
   box = addedNode.closest('.style-scope.ytd-watch-next-secondary-results-renderer');
-  console.log(box);
+  //console.log(box);
 
   var index;
 
   if (box !== null){
     index = [].indexOf.call(box.parentNode.children, box);
-    console.log(index);
+    console.log('(get_node_index) index: ' + index);
   }
 
   return index;
 }
 
+function generate_watch_tag(vidId) {
+    tag = '/watch?v=' + vidId;
+    //console.log(tag);
+    return tag
+};
+
+
 
 // Define callback function to get notified on changes
-function thumbnail_callback(array) {
+function thumbnail_callback() {
     // do something
-  alert('thumbnail callback');
+  //alert('thumbnail callback');
   console.log('callback');
 
 
     // Simulate a code delay
   setTimeout( function(){
     console.log('hello');
+    //console.log(img);
 
-    random_index = Math.floor(Math.random() * (3) + 1);
     img = document.querySelectorAll('#thumbnail #img')[random_index];
+    console.log(img);
     img.setAttribute('src', 'https://i.ytimg.com/vi/hY7m5jjJ9mM/default.jpg');
 
     console.log(img);
@@ -55,8 +74,6 @@ function thumbnail_callback(array) {
   }, 1000);
 
 
-  // console.log(videos.length);
-  //console.log(videos);
 }
 
 
@@ -100,15 +117,74 @@ function checkNode(addedNode) {
         // console.log(thumbnail_img_idx);
         // resetTimer();
         if (thumbnail_img_idx.length === 3) {
-          alert('thumbnails ready');
+          //alert('thumbnails ready');
           console.log(thumbnail_img_idx);
-          thumbnail_callback(thumbnail_img_idx);
+          thumbnail_callback();
         }
       }
     }
-      
+
+    if (addedNode.matches('.yt-simple-endpoint.inline-block.style-scope.ytd-thumbnail')) {
+      //console.log(addedNode);
+
+      index = get_node_index(addedNode);
+      if (index !== null && (0 < index) && (index < 4)) { 
+        thumbnail_src_idx++;
+
+        //thumbnail src callback
+        if (thumbnail_src_idx === 3) {
+            setTimeout(function(){
+              console.log('changing thumbnail href');
+              tag = generate_watch_tag('hY7m5jjJ9mM');
+
+              div = document.querySelectorAll('.yt-simple-endpoint.inline-block.style-scope.ytd-thumbnail')[random_index];
+              div.setAttribute('href', tag);
+
+              //console.log(div);
+
+            }, 1000);
+
+        }
+      }
+    }
+
+
+    if (addedNode.matches('#dismissable')) {
+      // console.log(addedNode);
+      // console.log(addedNode.querySelector('.yt-simple-endpoint.style-scope.ytd-compact-video-renderer'));
+
+      index = get_node_index(addedNode);
+      //console.log(index);
+      if (index !== null && (0 < index) && (index < 4)) { 
+        title_href_count++;
+
+        if (title_href_count === 3) {
+          console.log('changing title and src')
+          change_title();
+        }
+      }
+
+    }
+
+
   }
 }
+    //console.log(document.querySelector('.metadata.style-scope.ytd-compact-video-renderer'));
+
+function change_title() {
+      setTimeout(function(){
+        console.log('changing title href');
+
+        tag = generate_watch_tag('hY7m5jjJ9mM');        
+        div = document.querySelectorAll('.yt-simple-endpoint.style-scope.ytd-compact-video-renderer')[random_index];
+        console.log(div);
+        div.setAttribute('href', tag);
+
+
+
+      }, 5000);
+}
+
 
 // if (thumbnails_ready === true) {
 //   alert('true');
